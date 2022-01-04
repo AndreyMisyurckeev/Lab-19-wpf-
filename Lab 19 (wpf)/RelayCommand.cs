e96bf317277c3b93761d01b8a153bd60d17e74b4
AndreyMisyurckeev <91983861+AndreyMisyurckeev@users.noreply.github.com>
2022-01-04 17:35:40 +0300

    Add project files.

---
+++ Lab 19 (wpf)/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab_19__wpf_
+{
+    class RelayCommand:ICommand
+    {
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object> Execute,Func<object,bool> CanExecute=null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+    }
+}
